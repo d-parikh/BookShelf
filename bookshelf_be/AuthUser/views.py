08a71e8aa6950b151887f369a9cd976e7f67a483
d-parikh <parikhdhara151@gmail.com>
2023-12-26 11:06:44 +0530

    validate hashed password from database

--- bookshelf_be/AuthUser/views.py
+++ bookshelf_be/AuthUser/views.py
@@ -1,47 +1,52 @@
-# myapp/views.py
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from .serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser
+from django.contrib.auth.hashers import check_password
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        # Set the password using set_password to ensure proper hashing
+        user = serializer.save()
+        user.set_password(serializer.validated_data['password'])
+        user.save()
+
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
-        print("request***", request.data)
         if serializer.is_valid():
-            serializer.save()
+            self.perform_create(serializer)
             return Response({"message": "Successfully Registered. Login to access the App!"}, status=status.HTTP_201_CREATED)
         else:
-            print("request***222", request.data)
             return Response({"error": "User Already exists"}, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginUserView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
+    permission_classes = (permissions.AllowAny,)
+
     def post(self, request, *args, **kwargs):
         email = request.data.get('email', None)
         password = request.data.get('password', None)
-        print("email***", email)
-        print("password***", password)
 
         if email is None or password is None:
             return Response({'error': 'Both email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Retrieve the user from the database based on the email
         user = get_user_model().objects.filter(email=email).first()
-        print("user***", user)
 
-        # if user is None or not user.check_password(password):
-        #     return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'message': 'Login successful!'}, status=status.HTTP_200_OK)
-
-
+        if user is not None and check_password(password, user.password):
+            # Password matches, generate or retrieve token
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'message': 'Login successful!'}, status=status.HTTP_200_OK)
+        else:
+            # User with the specified email doesn't exist or password doesn't match
+            return Response({'error': 'Invalid email or password'}, status=status.HTTP_401_UNAUTHORIZED)
--- bookshelf_be/bookshelf_be/settings.py
+++ bookshelf_be/bookshelf_be/settings.py
@@ -54,9 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    
     #....
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'bookshelf_be.urls'
